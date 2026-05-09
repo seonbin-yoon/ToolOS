181127390964213a49099d391642ae2deb974ec6
Seonbin Yoon <seonbin.yoon0@gmail.com>
2026-05-09 17:33:00 +0900

    Refactor GetMemoryInfo to fix memory leaks and optimize logic

--- boot/utils/Memory.c
+++ boot/utils/Memory.c
@@ -11,29 +11,34 @@
 
 #include "TBL.h"
 
-void* Last_MemoryMapAddress = NULL;
-void* Last_ToolOSMemoryMapAddress = NULL;
+EFI_MEMORY_DESCRIPTOR *MemoryMap = NULL;
+VOID *InfoMemoryMap = NULL;
 
-EFI_STATUS Get_EFI_MemoryMap(IN TOOLOS_MASTER_MAP* BootInfo) {
+EFI_STATUS GetMemoryInfo(IN TOOLOS_MASTER_MAP* BootInfo) {
 	EFI_STATUS Status;
+
 	UINTN MemoryMapSize = 0;
-	EFI_MEMORY_DESCRIPTOR* MemoryMap = NULL;
 	UINTN MapKey = 0;
 	UINTN DescriptorSize = 0;
 	UINT32 DescriptorVersion = 0;
-	UINTN TOOLOSMemoryMapSize;
-	EFI_MEMORY_DESCRIPTOR* MPTR;
+
+	UINT64 MemoryMapNums = 0;
+	UINT64 ToolOSMMapSize = 0;
+	EFI_MEMORY_DESCRIPTOR *MPTR = NULL;
 
 	if (BootInfo == NULL || CompareMem(BootInfo->Signature, TOOLOS_INFOTABLE_Signature, 16) != 0) {
 		Status = EFI_INVALID_PARAMETER;
 		goto out;
 	}
 
-	if (Last_MemoryMapAddress || Last_ToolOSMemoryMapAddress) {
-		gBS->FreePool(Last_MemoryMapAddress);
-		gBS->FreePool(Last_ToolOSMemoryMapAddress);
-		Last_MemoryMapAddress = NULL;
-		Last_ToolOSMemoryMapAddress = NULL;
+	if (MemoryMap) {
+		gBS->FreePool(MemoryMap);
+		MemoryMap = NULL;
+	}
+
+	if (InfoMemoryMap) {
+		gBS->FreePool(InfoMemoryMap);
+		InfoMemoryMap = NULL;
 	}
 
 	Status = gBS->GetMemoryMap(
@@ -43,34 +48,33 @@ EFI_STATUS Get_EFI_MemoryMap(IN TOOLOS_MASTER_MAP* BootInfo) {
 		&DescriptorSize,
 		&DescriptorVersion
 	);
-
-	if (Status != EFI_BUFFER_TOO_SMALL) {
+	if (Status != EFI_BUFFER_TOO_SMALL)
 		goto out;
-	}
 
-	MemoryMapSize += DescriptorSize * 3;
-	BootInfo->T_MemoryMapInfo.MemoryMapNums = MemoryMapSize / DescriptorSize;
-	TOOLOSMemoryMapSize = sizeof(TOOLOS_MEMORY_MAP) * BootInfo->T_MemoryMapInfo.MemoryMapNums;
-
-	Status = gBS->AllocatePool(
-		EfiLoaderData,
-		TOOLOSMemoryMapSize,
-		(VOID **)&BootInfo->T_MemoryMap
-	);
-
-	if (EFI_ERROR(Status)) {
-		goto out;
-	}
+	MemoryMapNums = (MemoryMapSize / DescriptorSize) + 5;
+	MemoryMapSize += DescriptorSize * 5;
+	ToolOSMMapSize = sizeof(TOOLOS_MEMORY_MAP) * MemoryMapNums;
 
 	Status = gBS->AllocatePool(
 		EfiLoaderData,
 		MemoryMapSize,
 		(VOID **)&MemoryMap
 	);
+	if (EFI_ERROR(Status))
+		goto out;
 
+	Status = gBS->AllocatePool(
+		EfiLoaderData,
+		ToolOSMMapSize,
+		(VOID **)&BootInfo->T_MemoryMap
+	);
 	if (EFI_ERROR(Status)) {
+		gBS->FreePool(MemoryMap);
+		MemoryMap = NULL;
 		goto out;
 	}
+
+	InfoMemoryMap = BootInfo->T_MemoryMap;
 
 	Status = gBS->GetMemoryMap(
 		&MemoryMapSize,
@@ -79,30 +83,26 @@ EFI_STATUS Get_EFI_MemoryMap(IN TOOLOS_MASTER_MAP* BootInfo) {
 		&DescriptorSize,
 		&DescriptorVersion
 	);
-	
-	if (EFI_ERROR(Status)) {
+
+	if (MemoryMapNums < MemoryMapSize / DescriptorSize) {
+		Status = EFI_BUFFER_TOO_SMALL;
 		goto out;
 	}
+
+	MemoryMapNums = MemoryMapSize / DescriptorSize;
+	BootInfo->T_MemoryMapInfo.MemoryMapNums = MemoryMapNums;
+	BootInfo->T_MemoryMapInfo.MapKey = MapKey;
+	BootInfo->T_MemoryMapInfo.TotalMemorySize = 0;
 
 	MPTR = MemoryMap;
-	if (BootInfo->T_MemoryMapInfo.MemoryMapNums < MemoryMapSize / DescriptorSize) {
-		goto out;
-	}
-
-	BootInfo->T_MemoryMapInfo.MemoryMapNums = MemoryMapSize / DescriptorSize;
-
-	for (UINTN i = 0; i < BootInfo->T_MemoryMapInfo.MemoryMapNums; i++) {
-		BootInfo->T_MemoryMap[i].Type = MPTR->Type;
+	for (UINT64 i = 0; i < MemoryMapNums; i++) {
+		BootInfo->T_MemoryMap[i].NumberOfPages = MPTR->NumberOfPages;
 		BootInfo->T_MemoryMap[i].Reserved = 0;
 		BootInfo->T_MemoryMap[i].PhysicalStart = MPTR->PhysicalStart;
-		BootInfo->T_MemoryMap[i].NumberOfPages = MPTR->NumberOfPages;
+		BootInfo->T_MemoryMap[i].Type = MPTR->Type;
 		BootInfo->T_MemoryMapInfo.TotalMemorySize += MPTR->NumberOfPages * 4096;
 		MPTR = (EFI_MEMORY_DESCRIPTOR*)((UINT8*)MPTR + DescriptorSize);
-	};
-
-	BootInfo->T_MemoryMapInfo.MapKey = MapKey;
-	Last_MemoryMapAddress = MemoryMap;
-	Last_ToolOSMemoryMapAddress = BootInfo->T_MemoryMap;
+	}
 
 out:
 	return Status;
