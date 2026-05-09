fda9d935de9d2792457179625498cdd323a2b466
Seonbin Yoon <seonbin.yoon0@gmail.com>
2026-05-09 15:44:43 +0900

    Modify Function Definition

--- boot/include/TBL.h
+++ boot/include/TBL.h
@@ -36,7 +36,7 @@ EFI_STATUS
 );
 
 EFI_STATUS
-(Get_ACPI_Info)(
+(GetACPIInfo)(
 	IN TOOLOS_MASTER_MAP *BootInfo
 );
 
@@ -55,13 +55,7 @@ EFI_STATUS
 EFI_STATUS
 (ValidationELFHeader)(
 	IN EFI_FILE_PROTOCOL *File,
-	IN BOOLEAN Is64bit,
 	IN BOOLEAN IsBigEndian
-);
-
-EFI_STATUS
-(GetFileSize)(
-	IN EFI_FILE_PROTOCOL *File
 );
 
 extern const CHAR8 TOOLOS_INFOTABLE_Signature[16];
