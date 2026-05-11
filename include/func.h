d0a5a610dcaeba3dab872a8938bdd33f9183ad33
Seonbin Yoon <seonbin.yoon0@gmail.com>
2026-05-11 19:53:10 +0900

    containing kernel function definitions

---
+++ include/func.h
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Copyright (c) 2026 Seonbin Yoon
+ * Project: ToolOS
+ * File: func.h
+ * Description:
+ *  
+ */
+
+#ifndef FUNC_H
+#define FUNC_H
+
+#include "type.h"
+
+void (success_print)(struct TOOLOS_MASTER_MAP *boot_info, boolean is_success);
+
+#endif
