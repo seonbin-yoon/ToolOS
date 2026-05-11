a5ca9925828a1e8c7c5ba54ab01323a4aa73014c
Seonbin Yoon <seonbin.yoon0@gmail.com>
2026-05-11 19:55:53 +0900

    Implementing Basic Types for the Kernel

---
+++ include/type.h
@@ -0,0 +1,31 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Copyright (c) 2026 Seonbin Yoon
+ * Project: ToolOS
+ * File: type.h
+ * Description:
+ *  
+ */
+
+#ifndef TYPE_H
+#define TYPE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+
+typedef int64_t  i64;
+typedef int32_t  i32;
+typedef int16_t  i16;
+typedef int8_t   i8;
+
+typedef unsigned int   c32;
+typedef unsigned short c16;
+typedef char           c8;
+
+#endif
