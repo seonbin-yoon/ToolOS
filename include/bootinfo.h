9ce93eab263654510f5114718c3c53588da86156
Seonbin Yoon <seonbin.yoon0@gmail.com>
2026-05-12 19:54:18 +0900

    Unified using the revised kernel structure definitions

--- include/bootinfo.h
+++ include/bootinfo.h
@@ -15,7 +15,7 @@
 
 #pragma pack(1)
 // 8 + 1 + 6 + 1 + 4 + 4 + 8 + 1 + 3 = 36byte
-struct TOOLOS_ACPI_TABLE {
+typedef struct {
 	c8  Signature[8];
 	u8  Checksum;
 	c8  OEMID[6];
@@ -25,63 +25,62 @@ struct TOOLOS_ACPI_TABLE {
 	u64 XsdtAddress;
 	u8  ExtendedChecksum;
 	u8  Reserved[3];
-};
+} ACPI_RSDP_TABLE;
 #pragma pack()
 
 #pragma pack(1)
-// 4 + 4 + 8 + 8 = 24byte
-struct TOOLOS_MEMORY_MAP {
-	u32 Type;
-	u32 Reserved; // MUST BE 0
+// 8 + 8 + 8 + 8 = 32byte
+typedef struct {
+	u64 Type;
 	u64 PhysicalStart;
 	u64 NumberOfPages;
-};
+	u64 Attribute;
+} TOOLOS_MEMORY_MAP;
 #pragma pack()
 
 #pragma pack(1)
 // 8 + 8 + 8 = 24byte
-struct TOOLOS_MEMORY_MAPINFO {
+typedef struct {
 	u64 MemoryMapNums;
 	u64 MapKey;
 	u64 TotalMemorySize;
-};
-#pragma pack()
+} TOOLOS_MEMORY_MAPINFO;
 
 #pragma pack(1)
 // 4 + 4 + 4 + 4 = 16byte
-struct TOOLOS_PIXEL_BITMASK {
+typedef struct {
 	u32 RedMask;
 	u32 GreenMask;
 	u32 BlueMask;
 	u32 ReservedMask;
-};
+} TOOLOS_PIXEL_BITMASK;
 #pragma pack()
 
 #pragma pack(1)
 // 8 + 8 + 4 + 4 + 4 + 4 + 16 = 48byte
-struct TOOLOS_GRAPHICS_MAP {
-	u64                         FrameBufferBase;
-	u64                         FrameBufferSize;
-	u32                         Version;
-	u32                         HorizontalResolution;
-	u32                         VerticalResolution;
-	u32                         PixelsPerScanLine;
-	struct TOOLOS_PIXEL_BITMASK PixelInformation;
-};
+typedef struct {
+	u64                  FrameBufferBase;
+	u64                  FrameBufferSize;
+	u32                  Version;
+	u32                  HorizontalResolution;
+	u32                  VerticalResolution;
+	u32                  PixelsPerScanLine;
+	TOOLOS_PIXEL_BITMASK PixelInformation;
+} TOOLOS_GRAPHICS_MAP;
 #pragma pack()
 
-// 16 + 8 + 24 + 8 + 48 + 8 + 1 + 7 = 120byte
+// 16 + 8 + 24 + 8 + 48 + 8 + 1 + 7 = 128byte
 #pragma pack(1)
-struct TOOLOS_MASTER_MAP {
-	c8                           Signature[16];
-	u64                          KernelStartAddress;
-	struct TOOLOS_MEMORY_MAPINFO MemoryMapInfo;
-	struct TOOLOS_MEMORY_MAP     *MemoryMap;
-	struct TOOLOS_GRAPHICS_MAP   GraphicsMap;
-	struct TOOLOS_ACPI_TABLE     *ACPITable;
-	c8                           CheckSum;
-	u8                           Reserved[7];
-};
+typedef struct {
+	c8                    Signature[16];
+	u64                   KernelStartAddress;
+	TOOLOS_MEMORY_MAPINFO MemoryMapInfo;
+	TOOLOS_MEMORY_MAP*    MemoryMap;
+	TOOLOS_GRAPHICS_MAP   GraphicsMap;
+	ACPI_RSDP_TABLE*      RSDPTable;
+	u8                    Reserved[7];
+	bool                  SafeWritten;
+} TOOLOS_BOOTINFO_TABLE;
 #pragma pack()
 
 #endif
