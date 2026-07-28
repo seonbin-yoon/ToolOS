7b13c88cf6073a1ff3a64ea3db01ee3b76d965a9
Seonbin Yoon <seonbin.yoon0@gmail.com>
2026-07-28 05:51:09 +0000

    [style] Change the names of the structure and its members to snake_case

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -8,22 +8,22 @@
  *  
  */
 
-#include "bootinfo.h"
-#include "type.h"
-#include "func.h"
+#include <bootinfo.h>
+#include <types.h>
+#include <func.h>
 
-void kernel_main(struct TOOLOS_BOOTINFO_TABLE *boot_info) {
+void kernel_main(struct toolos_bootinfo_table *boot_info) {
 	success_print(boot_info, true);
 }
 
-void success_print(struct TOOLOS_BOOTINFO_TABLE *boot_info, bool is_success) {
-	u32 *gop = (u32 *)boot_info->GraphicsMap.FrameBufferBase;
+void success_print(struct toolos_bootinfo_table *boot_info, bool is_success) {
+	u32 *gop = (u32 *)boot_info->graphics_map.frame_buffer_base;
 
 	if (is_success) {
-		for (u64 i = 0; i < boot_info->GraphicsMap.PixelsPerScanLine * 5; i++)
+		for (u64 i = 0; i < boot_info->graphics_map.pixels_per_scan_line * 5; i++)
 			gop[i] = 0x00BFFF;
 	} else {
-		for (u64 i = 0; i < boot_info->GraphicsMap.PixelsPerScanLine * 5; i++)
+		for (u64 i = 0; i < boot_info->graphics_map.pixels_per_scan_line * 5; i++)
 			gop[i] = 0xFF0000;
 	}
 }
